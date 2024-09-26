c2f69baa9729cc95baeb1e839a608ad014e40acf
Ahmad-Zainudin-Fanani <azfann.natinons@gmail.com>
2024-09-26 09:34:40 +0700

    Pemilihan hari dengan If

---
+++ Praktikum05/PemilihanHariDenganIf.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PemilihanHariDenganIf<NoAbsen> {
+
+    public static void main(String[] args) {
+        // Deklarasi Scanner untuk menerima input dari pengguna
+        Scanner sc = new Scanner(System.in);
+
+        // Menerima input dari pengguna
+        System.out.print("Masukkan angka hari (1-7): ");
+        int hari = sc.nextInt();
+
+        // Memeriksa nilai input dan menampilkan hasil
+        if (hari >= 1 && hari <= 5) {
+            System.out.println("Weekday");
+        } else if (hari == 6 || hari == 7) {
+            System.out.println("Weekend");
+        } else {
+            System.out.println("Invalid Number");
+        }
+
+        // Menutup Scanner
+        sc.close();
+    }
+}
