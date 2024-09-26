808db19910ba6947263cf3876a69e340f9bdf924
Ahmad-Zainudin-Fanani <azfann.natinons@gmail.com>
2024-09-26 09:12:14 +0700

    pemilihan hari

--- Praktikum05/PemilihanHari02.java
+++ Praktikum05/PemilihanHari02.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
-/**
- * PemilihanHari02
- */
+
 public class PemilihanHari02 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        //Deklarasi variable dayName dan dayType bertipe String
+        
+        // Deklarasi variable dayName dan dayType bertipe String
         String dayName, dayType;
-        //perintah untuk membaca input
+
+        // Perintah untuk membaca input
         System.out.print("Input day name: ");
         dayName = sc.nextLine();
-        //Tambahkan switch case untuk mengelompokkan tipe hari
+
+        // Tambahkan switch case untuk mengelompokkan tipe hari
         switch (dayName.toLowerCase()) {
             case "monday":
             case "tuesday":
@@ -26,10 +27,12 @@ public class PemilihanHari02 {
                 break;
             default:
                 dayType = "invalid day name";
-            //Cetak hasil ke layar
-            System.out.println(dayName + "is a" + dayType);
-
-            sc.close();
         }
+
+        // Cetak hasil ke layar (diluar blok switch)
+        System.out.println(dayName + " is a " + dayType);
+
+        // Tutup scanner
+        sc.close();
     }
 }
