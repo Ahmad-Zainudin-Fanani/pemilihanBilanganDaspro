2ae69026fcbbe58dd6e49434c7f01ad0d0f92b0a
Ahmad-Zainudin-Fanani <azfann.natinons@gmail.com>
2024-09-26 08:44:12 +0700

    praktikum05

--- Praktikum05/PemilihanBilangan02.java
+++ Praktikum05/PemilihanBilangan02.java
@@ -1,24 +1,19 @@
 import java.util.Scanner;
-/**
- * PemilihanBilangan02
- */
+
 public class PemilihanBilangan02 {
 
     public static void main(String[] args) {
         // Deklarasi scanner
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
 
-        if (angka % 2 == 0)
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        }
-        else
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        // Menggunakan ternary operator untuk menentukan genap atau ganjil
+        String hasil = (angka % 2 == 0) ? "termasuk bilangan genap" : "termasuk bilangan ganjil";
+        
+        // Menampilkan hasil
+        System.out.println("Angka " + angka + " " + hasil);
 
         // Menutup scanner
         sc.close();
